4c40db5aed886481e879a1edef1bd9b156f5fb24
Masaru <s.masaru.dev@gmail.com>
2024-04-30 10:27:46 +0900

    Update content_main.js

--- content_main.js
+++ content_main.js
@@ -1,7 +1,7 @@
 import User from './models/user.js';
 
 export function main() {
-  const user = User.new({name: 'mshibata-content_main'});
+  const user = User.new({name: 'content_main'});
   console.log(user.greet());
   console.log(
     "Is chrome.runtime available here?",
